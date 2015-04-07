7f45b7cc9b7bb073370158833b556f091c7f8cc6
Pedro <pedro@epow.me>
2015-04-07 17:15:38 +0100

    Port setup.py from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
-from distutils.core import setup
+from setuptools import setup,find_packages
 
+    
 setup(
-    name='django-crud',
+    name='django-simplecrud',
+
     version='0.1',
     description='CRUD framework for Django',
+    keywords='django crud',
     author='Pedro Tavares',
     author_email='pedro@ermapp.net',
     url='https://github.com/ptav/django-crud',
-    packages=['crud'],
+    license='LICENSE',
+
+    packages=find_packages(),
+    
+    include_package_data=True,
+
+    #install_requires=[
+    #    "Django >= 1.4.0"
+    #],
 )

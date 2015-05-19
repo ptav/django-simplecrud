b11cbc63020f1000101f75d1451a01708c218896
Pedro <pedro@epow.me>
2015-05-19 20:49:30 +0100

    Update release number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup,find_packages
 setup(
     name='django-simplecrud',
 
-    version='1.0b12',
+    version='1.0b13',
     description='CRUD framework for Django',
     long_description="A simple Create-Review-Update-Delete framework for Django. Ready to use out-of-the-box: includes both views and templates",
     keywords='django, crud, views, templates',
